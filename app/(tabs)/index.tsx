cb4ef927f8ad0db0a1fc1447c939b3c3658afb06
Shalini <shalinibadiginchala24@gmail.com>
2026-04-06 18:37:58 +0530

    Initial commit

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,17 @@
+import { Text, View } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Text style={{ fontSize: 24, fontWeight: 'bold' }}>
+        Kiddocare App 🚀
+      </Text>
+    </View>
+  );
+}
